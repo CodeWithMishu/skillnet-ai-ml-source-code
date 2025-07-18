cd26cd32d8b25f3718c1fde0d374bbdb2397c36d
codewithmishu <codewithmishu@gmail.com>
2025-07-18 11:31:20 +0530

    Add HTML error page for 403 Forbidden response

---
+++ 07_functions/02_second.py
@@ -0,0 +1,30 @@
+# default parameter values
+# def my_function(country="india"):
+#     print("i am from " + country)
+# my_function("Thailand")
+# my_function()
+# my_function("WB")
+
+# def my_function(food):
+#     for x in food:
+#         print(x) 
+# fruits = ["apple","banana","cherry"]
+# my_function(fruits)
+
+# def my_function(X):
+#     return 10+X
+# my_function(5)
+# my_function(6)
+# my_function(3)
+# print(my_function(33))
+
+
+# def my_function():
+    # 
+
+# def my_function(x,/):
+#     print(x)
+# my_function(3)
+
+def my_function(x):
+    print(my_function(5))
