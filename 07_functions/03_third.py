e909bdb4f7d8de132d790b1c692721d7920fd6a1
codewithmishu <codewithmishu@gmail.com>
2025-07-19 20:20:12 +0530

    Remove commented-out palindrome function and related input code

--- 07_functions/03_third.py
+++ 07_functions/03_third.py
@@ -11,19 +11,21 @@
 #     print(f"{word} is not a palindrome")
 
 import random
-def guess_number(secret, attempts=1):
-     while True:
-             guess=int(input(f"Attempt {attempts}: Guess the number (between 1 and 10): "))
-             if guess <secret:
-              print("Too low! Try again.")
-             return guess_number(secret, attempts + 1)
-             elif guess>secret:
-             print("Too high! Try again.")
-             return guess_number(secret, attempts + 1)
-            elif guess==secret:
-             print(f"Congratulations! You've guessed the number {secret} in {attempts} attempts.")
-            return attempts
-            
 
+def guess_number(secret, attempts=1):
+ while True:
+        guess=int(input(f"Attempt {attempts}: Guess the number (between 1 and 10): "))
+        if attempts == 10:
+         break
+        if guess <secret:
+         print("Too low! Try again.")
+         return guess_number(secret, attempts + 1)
+        elif guess>secret:
+         print("Too high! Try again.")
+         return guess_number(secret, attempts + 1)
+        elif guess==secret:
+         print(f"Congratulations! You've guessed the number {secret} in {attempts} attempts.")
+        return attempts
+            
 
 guess_number(random.randint(1,10))
