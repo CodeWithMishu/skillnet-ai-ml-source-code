ec20355cf557df403776abd36c0af0add35dede9
codewithmishu <codewithmishu.com>
2025-07-21 21:34:39 +0530

    Add Jupyter notebook for built-in functions on lists and create demo script for list manipulation
    - Created a Jupyter notebook demonstrating various built-in functions on lists, including sorting, counting, and calculating max/min values.
    - Implemented a demo Python script that defines a function to double the elements of a list and demonstrates list comprehension for the same operation.

---
+++ demo.py
@@ -0,0 +1,9 @@
+def my_function(list):
+    for i in list:
+        return i*2
+print(my_function(list))
+list={1,2,3,4,5}
+multiple_list = []
+for i in list:
+    multiple_list.append(i*2)
+print(multiple_list)
